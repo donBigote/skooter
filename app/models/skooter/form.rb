114f50fb851b0eabda334f7a29643a51531f4e77
Juan Trejo <trejocola@gmail.com>
2018-01-24 10:43:52 -0600

    adding paperclip validations by exclude files that does not be a xml file or exeding premitted size

--- app/models/skooter/form.rb
+++ app/models/skooter/form.rb
@@ -1,6 +1,8 @@
 module Skooter
   class Form < ApplicationRecord
     has_attached_file :document
-    validates_attachment_content_type :document, content_type: 'application/xml'
+    validates_attachment_content_type :document,
+                                      content_type: 'application/xml'
+    validates_attachment_size :document, in: 0..20.kilobytes
   end
 end
