84e6f1a9321fa0187eae027d038f8265d2d117ee
Juan Trejo <trejocola@gmail.com>
2018-01-30 13:52:08 -0600

    changes in initializer and settings

--- app/models/skooter/form.rb
+++ app/models/skooter/form.rb
@@ -1,8 +1,21 @@
 module Skooter
   class Form < ApplicationRecord
-    has_attached_file :document
+    has_attached_file :document # ,
+                      # storage: :s3,
+                      # s3_credentials: proc { |a| a.instance.s3_credentials },
+                      # s3_permissions: :private
+
     validates_attachment_content_type :document,
                                       content_type: 'application/xml'
     validates_attachment_size :document, in: 0..1024.kilobytes
+
+    def s3_credentials
+      {
+        bucket: ENV['S3_BUCKET_NAME'],
+        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+        s3_region: ENV['AWS_REGION']
+      }
+    end
   end
 end
