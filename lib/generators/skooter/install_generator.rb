f0e50433403aa7119086682db3c3bbdd3b08221a
Juan Trejo <trejocola@gmail.com>
2018-02-01 14:34:44 -0600

    adding cors config for generator

--- lib/generators/skooter/install_generator.rb
+++ lib/generators/skooter/install_generator.rb
@@ -16,6 +16,24 @@ module Skooter
         route "mount Skooter::Engine => '/skooter'"
       end
 
+      def setup_rails_cors
+        inject_into_file "config/application.rb", cors_code, after: "class Application < Rails::Application\n"
+      end
+
+      def cors_code
+<<RUBY
+  # Rails 5
+  # this config area works with rack-cors gem, and allow requests to the API by
+  # any client provided that the requests has to be requested by the ElectronApp
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins "*"
+      resource "*", headers: :any, methods: [:get, :post, :options]
+    end
+  end
+RUBY
+      end
+
       def copy_initializer_and_config
         copy_file 'skooter.rb', 'config/initializers/skooter.rb'
         copy_file 'skooter_config.yml', 'config/skooter_config.yml'
